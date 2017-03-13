c786b2bdb96849d05b9b868bb4f596b2a2c536ea
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-03-13 22:08:02 +0100

    Make a constructor function

--- dice.js
+++ dice.js
@@ -2,11 +2,12 @@
  * Created by vagrant on 3/12/17.
  */
 
-
-var dice = {
-    sides:  6,
-    roll: function () {
+function Dice (sides) {
+    this.sides = sides;
+    this.roll = function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
    return randomNumber;
+    }
 }
-}
+
+var dice = new Dice(6);
